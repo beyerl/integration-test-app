952c1743345a5ca0694d494cd3e0388b19c6cf4e
lorenz.beyer@orca-software.com <lorenz.beyer@orca-software.com>
2022-01-19 16:48:16 +0100

    fixe basehref

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { legacyRoutingSegment } from './constants/config';
 import { IntegrationV1EndpointComponent } from './view-logic/integrationv1-endpoint/integrationv1-endpoint.component';
 import { IntegrationV2EndpointComponent } from './view-logic/integrationv2-endpoint/integrationv2-endpoint.component';
 
+const baseHref = "integration-test-app"
+
 const routes: Routes = [
-    { path: `${legacyRoutingSegment}/:integrationType`, component: IntegrationV1EndpointComponent },
-    { path: ':integrationType/:usecase', component: IntegrationV2EndpointComponent },
-    { path: '', redirectTo: `${IIntegrationType.Catalog}/${IUsecase.Default}`, pathMatch: 'full' },
-    { path: '**', redirectTo: `${IIntegrationType.Catalog}/${IUsecase.Default}` }
+    { path: `${baseHref}/${legacyRoutingSegment}/:integrationType`, component: IntegrationV1EndpointComponent },
+    { path: `${baseHref}/:integrationType/:usecase`, component: IntegrationV2EndpointComponent },
+    { path: `${baseHref}/`, redirectTo: `${IIntegrationType.Catalog}/${IUsecase.Default}`, pathMatch: 'full' },
+    { path: `${baseHref}/**`, redirectTo: `${IIntegrationType.Catalog}/${IUsecase.Default}` }
 ];
 
 // configures NgModule imports and exports
