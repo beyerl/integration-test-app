23b9093434a8a0490564b3ab7b063292723d8f6d
lorenz.beyer@orca-software.com <lorenz.beyer@orca-software.com>
2022-01-19 16:40:10 +0100

    fix base url

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ng build --base-href="https://beyerl.github.io/integration-test-app" &&
+ng build --base-href="https://beyerl.github.io/integration-test-app/" &&
 git add -A &&
 git commit -m "build and deploy" &&
 git push &&
