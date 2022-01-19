f6d9152ae62c297a0d28073dcb0776213fca96dd
lorenz.beyer@orca-software.com <lorenz.beyer@orca-software.com>
2022-01-19 17:15:12 +0100

    change to hash locationstrategy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,16 @@ import { EndpointComponent } from './view-logic/endpoint/endpoint.component';
 import { EndpointNavComponent } from './view-logic/endpoint-nav/endpoint-nav.component';
 import { IntegrationV1EndpointComponent } from './view-logic/integrationv1-endpoint/integrationv1-endpoint.component';
 import { IntegrationV2EndpointComponent } from './view-logic/integrationv2-endpoint/integrationv2-endpoint.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, BrowserAnimationsModule,
     BsDropdownModule.forRoot(), CollapseModule.forRoot(), AppRoutingModule],
   declarations: [AppComponent, NavbarComponent, IframeComponent, EndpointComponent, EndpointNavComponent, IntegrationV1EndpointComponent, IntegrationV2EndpointComponent],
   bootstrap: [AppComponent],
-  providers: [{ provide: APP_BASE_HREF, useValue: '/integration-test-app' }]
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    { provide: APP_BASE_HREF, useValue: '/integration-test-app' }
+  ],
 })
 export class AppModule { }
